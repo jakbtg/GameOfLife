3181883e96b4a0cc20d630eb1446d47c3e43f3a9
Giacomo Grandi <jak.grandi@gmail.com>
2022-06-09 00:07:55 +0200

    idea: try use tkinter to build app

--- src/GameOfLife.py
+++ src/GameOfLife.py
@@ -1,8 +1,7 @@
-# This is the implementation of the Conway's Game of Life.
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import tkinter as tk
 
 
 class GameOfLife:
